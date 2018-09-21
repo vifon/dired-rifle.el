5090275b2e5a200d087f51efc06008a4ac7a0060
Wojciech Siewierski <wojciech.siewierski@onet.pl>
2018-09-22 00:38:47 +0200

    Add a documentation

--- dired-rifle.el
+++ dired-rifle.el
@@ -25,7 +25,7 @@
 ;; Run rifle(1) known from ranger <https://github.com/ranger/ranger>
 ;; from a dired buffer.
 
-;; Press 'r' to open the current file.  See the `dired-rifle' function
+;; Press 'r' to open the current file.  See the `dired-rifle' command
 ;; documentation for more running options.
 
 ;;; Code:
